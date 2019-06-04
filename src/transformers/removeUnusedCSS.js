f4c0d568658fff403cdee7e6dfec0a8b89c087d5
Cosmin Popovici <hellocosmin@gmail.com>
2019-06-04 20:01:26 +0300

    Update email-comb.

--- src/transformers/removeUnusedCSS.js
+++ src/transformers/removeUnusedCSS.js
@@ -1,11 +1,11 @@
-const emailComb = require('email-comb')
+const { comb } = require('email-comb')
 
 module.exports = async (html, config) => {
 
   let opts = config.cleanup.removeUnusedCSS
 
   if (config.cleanup && opts && opts.enabled) {
-    return emailComb(html, opts).result
+    return comb(html, opts).result
   }
 
   return html

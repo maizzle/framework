a5e7e4aab935342ef5133157ca27b287121e1c45
Cosmin Popovici <hellocosmin@gmail.com>
2019-07-16 20:15:20 +0300

    Don't add baseImageURL to empty attributes.

--- src/transformers/applyBaseImageUrl.js
+++ src/transformers/applyBaseImageUrl.js
@@ -5,8 +5,8 @@ module.exports = async (html, config) => {
   const url = config.baseImageURL
 
   if (url && isUrl(url)) {
-    return html.replace(/(background="|src=")(?!https?:\/\/)\/?/ig, '$1' + url)
-      .replace(/(background(-image)?:\s?url\('?)(?!'?https?:\/\/)\/?/ig, '$1' + url)
+    return html.replace(/(background="|src=")(?!\s+|url\('?'?\)|"|https?:\/\/)\/?/ig, '$1' + url)
+      .replace(/(background(-image)?:\s?url\('?)(?!['\)]|https?:\/\/)\/?/ig, '$1' + url)
   }
 
   return html

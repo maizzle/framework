23ab1a1a9d92df935d70724c5d3ac9eae4d01c93
Cosmin Popovici <hellocosmin@gmail.com>
2019-12-19 19:26:26 +0200

    Make sure Nunjucks config exists before using it.
    Fixes #77.

--- src/nunjucks/index.js
+++ src/nunjucks/index.js
@@ -4,9 +4,10 @@ const markdown = require('nunjucks-markdown')
 const ComponentExtension = require('../nunjucks/tags/component')
 
 module.exports.init = async (config) => {
-  const tags = config.tags || {}
+  const tags = config && config.tags ? config.tags : {}
+  const basePath = config && config.path ? config.path : process.cwd()
 
-  const nunjucksEnv = nunjucks.configure(`${config.path || process.cwd()}`, { autoescape: false, noCache: true, tags: tags })
+  const nunjucksEnv = nunjucks.configure(basePath, { autoescape: false, noCache: true, tags: tags })
 
   nunjucksEnv.addExtension('componentExtension', new ComponentExtension(nunjucks))
 

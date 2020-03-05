ecc45478f7097fee253a4b7adc4805a15c18013f
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-05 18:41:36 +0200

    Replace dots with underscores in class names.
    Fixes #111.

--- src/transformers/safeClassNames.js
+++ src/transformers/safeClassNames.js
@@ -4,14 +4,16 @@ module.exports = async (html, env) => {
       // Rewrite class names in `<head>` CSS
       .replace(/(\..+)(\\:|\\\/|\\%|\\\.)/g, group => {
         return group
-          .replace(/\\:|\\\/|\\\./g, '-') // replace `\/` and `\:` with `-`
-          .replace(/\\%/g, 'pc') // replace `%` with `pc`
+          .replace(/\\:|\\\//g, '-') // replace `\/` and `\:` with `-`
+          .replace(/\\%/g, 'pc') // replace `\%` with `pc`
+          .replace(/\\\./g, '_') // replace `\.` with `_`
       })
       // Rewrite class names in `<body>` HTML
       .replace(/class\s*=\s*["'][^"']*[/:][^"']*["']/g, group => {
         return group
-          .replace(/\/|:|\./g, '-') // replace `\/` and `\:` with `-`
-          .replace(/%/g, 'pc')
+          .replace(/\/|:/g, '-') // replace `\/` and `\:` with `-`
+          .replace(/%/g, 'pc') // replace `%` with `pc`
+          .replace(/\./g, '_') // replace `.` with `_`
       })
   }
 

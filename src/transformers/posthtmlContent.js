a274d5712c87180b0bbd1800666f312eca4e13de
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-25 17:43:14 +0200

    Update posthtml-content.
    Object keys order issue was fixed, no need to manually sort them.

--- src/transformers/posthtmlContent.js
+++ src/transformers/posthtmlContent.js
@@ -3,14 +3,8 @@ const Tailwind = require('../generators/tailwind')
 const posthtmlContent = require('posthtml-content')
 
 module.exports = async (html, config) => {
-  let replacements = config.transformContents || {}
+  const replacements = config.transformContents || {}
   replacements.postcss = (css) => Tailwind.fromString(css, html, false, config)
-
-  replacements = Object.keys(replacements)
-    .sort()
-    .reduce((acc, key) => ({
-      ...acc, [key]: replacements[key]
-    }), {})
 
   html = await posthtml([posthtmlContent(replacements)]).process(html).then(res => res.html)
 

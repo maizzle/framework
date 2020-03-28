5e71acd334451aaa9691015f0005e5eb1558d5df
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-28 18:32:59 +0200

    Use PostHTML for preferBgColorAttribute.

--- src/transformers/removeInlineBgcolor.js
+++ src/transformers/removeInlineBgcolor.js
@@ -1,13 +1,39 @@
-const cheerio = require('cheerio')
+const posthtml = require('posthtml')
+const parseAttrs = require('posthtml-attrs-parser')
 
-module.exports = async (html, config) => {
-  if (config.cleanup && config.cleanup.preferBgColorAttribute) {
-    const $ = cheerio.load(html, { decodeEntities: false })
-    $('[bgcolor]').each((i, el) => {
-      $(el).css('background-color', '')
+const removeInlineBGColor = (options = {}) => tree => {
+  options.tags = options.tags || ['body', 'marquee', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr']
+
+  const process = node => {
+    if (!options.tags.includes(node.tag)) return node
+
+    const attrs = parseAttrs(node.attrs, {
+      rules: {
+        bgcolor: {
+          delimiter: /\s+/,
+          glue: ' '
+        }
+      }
     })
 
-    return $.html()
+    if (attrs.style && attrs.style['background-color']) {
+      attrs.bgcolor = attrs.style['background-color']
+      delete attrs.style['background-color']
+      node.attrs = attrs.compose()
+      node.attrs.style = node.attrs.style.substr(-1) === ';' ? node.attrs.style : node.attrs.style + ';'
+    }
+
+    return node
+  }
+
+  return tree.walk(process)
+}
+
+module.exports = async (html, config) => {
+  const prefers = config.cleanup.preferBgColorAttribute
+  const tags = prefers ? prefers.tags : []
+  if (prefers || (prefers && prefers.enabled)) {
+    html = await posthtml([removeInlineBGColor({ tags: tags })]).process(html).then(response => response.html)
   }
 
   return html

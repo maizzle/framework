f8588bf0412d2364939e757fe89b9e427dcacd49
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-30 12:21:12 +0300

    fix(transformers): switch to posthtml-md2html

--- src/transformers/markdown.js
+++ src/transformers/markdown.js
@@ -1,6 +1,6 @@
 const posthtml = require('posthtml')
-const markdown = require('posthtml-markdown')
+const markdown = require('posthtml-md2html')
 
 module.exports = async (html, config) => {
-  return posthtml([markdown(config.markdown)]).process(html).then(res => res.html)
+  return posthtml([markdown(config.markdown)]).process(html).then(result => result.html)
 }

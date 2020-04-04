a758360375f679eb2c95696cb265e6f3a7e15a81
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-04 14:35:36 +0300

    fix(transformers): better type check for safeClassNames

--- src/transformers/safeClassNames.js
+++ src/transformers/safeClassNames.js
@@ -3,7 +3,7 @@ const { getPropValue } = require('../utils/helpers')
 const safeClassNames = require('posthtml-safe-class-names')
 
 module.exports = async (html, config) => {
-  if (config.env !== 'local') {
+  if (typeof config.env === 'string' && config.env !== 'local') {
     const replacements = config.safeClassNames || {}
     const options = getPropValue(config, 'build.posthtml.options') || {}
 

e23f7fd778ce3ae7a067f790dc6a008e071967dd
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-05 17:18:27 +0300

    fix(transformers): better type check for minifier

--- src/transformers/minify.js
+++ src/transformers/minify.js
@@ -1,7 +1,8 @@
 const { crush } = require('html-crush')
+const { isObject } = require('../utils/helpers')
 
 module.exports = async (html, config) => {
-  if (config.minify && config.minify.enabled) {
+  if (isObject(config.minify) && config.minify.enabled) {
     html = crush(html, config.minify).result
   }
 

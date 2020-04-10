ebcb5c6b21ef06b31783b245b7467ccd27811dae
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-10 18:25:18 +0300

    style: add xo config

---
+++ xo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  space: true,
+  rules: {
+    semi: 0,
+    'unicorn/string-content': 0,
+    'promise/prefer-await-to-then': 0,
+    'unicorn/no-abusive-eslint-disable': 0,
+    quotes: ['error', 'single', {allowTemplateLiterals: true}]
+  }
+}

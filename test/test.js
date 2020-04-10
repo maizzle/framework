59caa38ee1379bb24c2f21742764ca2bb83991fa
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-10 18:23:05 +0300

    test(tostring): should compile html string

---
+++ test/test.js
@@ -0,0 +1,23 @@
+const test = require('ava')
+const Maizzle = require('../src')
+
+const {join} = require('path')
+const {readFileSync} = require('fs')
+
+const fixture = file => readFileSync(join(__dirname, 'fixtures', `${file}.html`), 'utf8')
+const expected = file => readFileSync(join(__dirname, 'expected', `${file}.html`), 'utf8')
+
+const clean = html => html.replace(/[^\S\r\n]+$/gm, '').trim()
+const env = {maizzle: {config: {env: 'node'}}}
+
+const renderString = (string, options = {}) => Maizzle.render(string, {...env, ...options}).then(html => html)
+
+const processFile = (t, name, options = {}, log = false) => {
+  return renderString(fixture(name), {...env, ...options})
+    .then(result => log ? console.log(result) : clean(result))
+    .then(html => t.is(html, expected(name).trim()))
+}
+
+test('It compiles HTML string if no options are passed', t => {
+  return processFile(t, 'basic')
+})

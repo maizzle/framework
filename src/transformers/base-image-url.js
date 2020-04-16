76911a985bb5688a14d5e54a388fc2e02f9ed0c6
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-16 21:43:57 +0300

    feat(transformers): allow using relative paths in baseImageURL

--- src/transformers/base-image-url.js
+++ src/transformers/base-image-url.js
@@ -1,9 +1,7 @@
-const isUrl = require('is-url-superb')
-
 module.exports = async (html, config) => {
   const url = config.baseImageURL
 
-  if (url && isUrl(url)) {
+  if (url) {
     return html.replace(/(background="|src=")(?!\s+|url\('?'?\)|"|https?:\/\/)\/?/gi, '$1' + url)
       .replace(/(background(-image)?:\s?url\('?)(?![')]|https?:\/\/)\/?/gi, '$1' + url)
   }

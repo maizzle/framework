b36201cc122a791e5d02ac04222dfba61bbf405f
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-21 17:12:28 +0300

    feat(transformers): add default extra attributes

--- src/transformers/extra-attributes.js
+++ src/transformers/extra-attributes.js
@@ -1,24 +1,35 @@
 const cheerio = require('cheerio')
-const {isEmptyObject} = require('../utils/helpers')
+const {isObject} = require('../utils/helpers')
 
 module.exports = async (html, config) => {
-  if (config.extraAttributes && !isEmptyObject(config.extraAttributes)) {
-    const $ = cheerio.load(html, {decodeEntities: false})
-
-    Object.entries(config.extraAttributes).forEach(([element, attrs]) => {
-      Object.entries(attrs).forEach(attr => {
-        const $element = $(element)
-        const [name, value] = attr
-        if (name === 'class') {
-          return $element.addClass(value)
-        }
-
-        return $element.attr(name, value)
-      })
-    })
-
-    return $.html()
+  let attributes = {
+    table: {
+      cellpadding: 0,
+      cellspacing: 0,
+      role: 'presentation'
+    },
+    img: {
+      alt: ''
+    }
   }
 
-  return html
+  if (isObject(config.extraAttributes)) {
+    attributes = {...attributes, ...config.extraAttributes}
+  }
+
+  const $ = cheerio.load(html, {decodeEntities: false})
+
+  Object.entries(attributes).forEach(([element, attrs]) => {
+    Object.entries(attrs).forEach(attr => {
+      const $element = $(element)
+      const [name, value] = attr
+      if (name === 'class') {
+        return $element.addClass(value)
+      }
+
+      return $element.attr(name, value)
+    })
+  })
+
+  return $.html()
 }

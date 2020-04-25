fd0bfe02f24f3f76bc2dbea82e4506b5c53ac52d
Cosmin Popovici <hellocosmin@gmail.com>
2020-04-25 14:23:51 +0300

    feat: use other plugins in modules

--- src/generators/posthtml.js
+++ src/generators/posthtml.js
@@ -10,21 +10,36 @@ const {getPropValue} = require('../utils/helpers')
 
 module.exports = async (html, config) => {
   const layoutsOptions = getPropValue(config, 'build.layouts') || {}
-  const modulesOptions = getPropValue(config, 'build.components') || {}
+
   const includeOptions = getPropValue(config, 'build.includes') || {}
-  const fetchOptions = getPropValue(config, 'build.posthtml.fetch') || {}
+  const includePlugin = includes({...includeOptions})
+
   const outlookOptions = getPropValue(config, 'build.posthtml.outlook') || {}
+  const outlookPlugin = outlook({...outlookOptions})
+
+  const fetchOptions = getPropValue(config, 'build.posthtml.fetch') || {}
+  const fetchPlugin = fetch({...fetchOptions})
+
+  const modulesOptions = getPropValue(config, 'build.components') || {}
+
   const posthtmlOptions = getPropValue(config, 'build.posthtml.options') || {}
   const posthtmlPlugins = getPropValue(config, 'build.posthtml.plugins') || []
+
   const expressionsOptions = getPropValue(config, 'build.posthtml.expressions') || {}
+  const expressionsPlugin = expressions({...expressionsOptions, locals: {page: config}})
 
   return posthtml([
     layouts({strict: false, ...layoutsOptions}),
-    includes({...includeOptions}),
-    outlook({...outlookOptions}),
-    fetch({...fetchOptions}),
-    modules({tag: 'component', attribute: 'src', ...modulesOptions}),
-    expressions({...expressionsOptions, locals: {page: config}}),
+    includePlugin,
+    outlookPlugin,
+    fetchPlugin,
+    modules({tag: 'component', attribute: 'src', plugins: [
+      includePlugin,
+      outlookPlugin,
+      fetchPlugin,
+      expressionsPlugin
+    ], ...modulesOptions}),
+    expressionsPlugin,
     ...posthtmlPlugins
   ])
     .process(html, {...posthtmlOptions})

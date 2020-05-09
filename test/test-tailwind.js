1b1fa047ea1e67f0aa02b0113696e8a3dff15ff6
Cosmin Popovici <hellocosmin@gmail.com>
2020-05-09 23:39:31 +0300

    test(tailwind): trim css result
    helps avoid crlf/lf line ending issues

--- test/test-tailwind.js
+++ test/test-tailwind.js
@@ -12,7 +12,7 @@ test('uses Tailwind defaults if no config specified', async t => {
   const css = await Tailwind.compile('', '', {}, config)
 
   t.not(css, undefined)
-  t.is(css, '@media (min-width: 1280px) {\n\n  .xl\\:z-0 {\n    z-index: 0\n  }\n}')
+  t.is(css.trim(), '@media (min-width: 1280px) {\n\n  .xl\\:z-0 {\n    z-index: 0\n  }\n}')
 })
 
 test('uses CSS file provided in environment config', async t => {
@@ -28,7 +28,7 @@ test('uses CSS file provided in environment config', async t => {
   const css = await Tailwind.compile('', '<div class="text-center foo">test</div>', {}, config)
 
   t.not(css, undefined)
-  t.is(css, '.text-center {\n  text-align: center;\n}\n\n.foo {\n  color: red;\n}\n')
+  t.is(css.trim(), '.text-center {\n  text-align: center;\n}\n\n.foo {\n  color: red;\n}')
 })
 
 test('uses purgeCSS options provided in the config', async t => {
@@ -41,7 +41,7 @@ test('uses purgeCSS options provided in the config', async t => {
 
   const css = await Tailwind.compile('', '', {}, config)
 
-  t.is(css, '.z-0 {\n  z-index: 0\n}\n\n.z-10 {\n  z-index: 10\n}')
+  t.is(css.trim(), '.z-0 {\n  z-index: 0\n}\n\n.z-10 {\n  z-index: 10\n}')
 })
 
 test('uses postcss plugins from the config when compiling from string', async t => {
@@ -59,5 +59,5 @@ test('uses postcss plugins from the config when compiling from string', async t 
   const css = await Tailwind.compile('.test {transform: scale(0.5)}', '<div class="test">Test</a>', {}, maizzleConfig)
 
   t.not(css, undefined)
-  t.is(css, '.test {\n  -webkit-transform: scale(0.5);\n      -ms-transform: scale(0.5);\n          transform: scale(0.5)\n}')
+  t.is(css.trim(), '.test {\n  -webkit-transform: scale(0.5);\n      -ms-transform: scale(0.5);\n          transform: scale(0.5)\n}')
 })

224c85b3cac2f909c9de3451ba4cffe089b503c1
Cosmin Popovici <hellocosmin@gmail.com>
2020-06-08 18:13:24 +0300

    build: disable unicorn/no-reduce

--- xo.config.js
+++ xo.config.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     semi: 0,
     complexity: 0,
+    'unicorn/no-reduce': 0,
     'unicorn/string-content': 0,
     'promise/prefer-await-to-then': 0,
     'unicorn/no-abusive-eslint-disable': 0,

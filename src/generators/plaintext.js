8111b19e2ddb285ae79ff3089c1c85535b73ae6d
Cosmin Popovici <hellocosmin@gmail.com>
2020-10-31 16:44:47 +0200

    feat(core): plaintext config
    pass `plaintext` config object to string-strip-html

--- src/generators/plaintext.js
+++ src/generators/plaintext.js
@@ -5,15 +5,14 @@ const {getPropValue} = require('../utils/helpers')
 module.exports.generate = async (html, destination, config) => {
   destination = getPropValue(config, 'permalink') || destination
 
-  const plaintext = stripHTML(html,
-    {
-      dumpLinkHrefsNearby: {
-        enabled: true,
-        putOnNewLine: true,
-        wrapHeads: '[',
-        wrapTails: ']'
-      }
-    }).result
+  const options = getPropValue(config, 'plaintext') || {}
+
+  const plaintext = stripHTML(html, {
+    dumpLinkHrefsNearby: {
+      enabled: true
+    },
+    ...options
+  }).result
 
   if (typeof destination === 'string') {
     destination = path.join(path.dirname(destination), path.basename(destination, path.extname(destination)) + '.txt')

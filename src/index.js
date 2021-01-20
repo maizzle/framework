c4087336ee044deb8db2210b59862082b30ce566
Cosmin Popovici <hellocosmin@gmail.com>
2021-01-20 18:24:15 +0200

    refactor: file watching
    use chokidar to watch templates separate from other files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const ora = require('ora')
 const path = require('path')
 const fs = require('fs-extra')
-const bs = require('browser-sync')
+const bs = require('browser-sync').create()
 const {get, merge} = require('lodash')
 const Output = require('./generators/output')
 const transformers = require('./transformers')
 const Plaintext = require('./generators/plaintext')
+const chokidar = require('chokidar')
 
 const self = module.exports = { // eslint-disable-line
   ...transformers,
+  render: async (html, options) => Output.toString(html, options),
+  plaintext: async (html, config = {}) => Plaintext.generate(html, false, config),
   build: async (env = 'local', config = {}) => {
     const start = new Date()
     const spinner = ora('Building emails...').start()
@@ -22,8 +25,6 @@ const self = module.exports = { // eslint-disable-line
         throw error
       })
   },
-  render: async (html, options) => Output.toString(html, options),
-  plaintext: async (html, config = {}) => Plaintext.generate(html, false, config),
   serve: async config => {
     await self
       .build('local', config)
@@ -36,14 +37,17 @@ const self = module.exports = { // eslint-disable-line
             let templates = get(config, 'build.templates')
             templates = Array.isArray(templates) ? templates : [templates]
 
-            const baseDir = get(templates[0], 'destination.path', 'build_local')
+            // File watchers
+            const templatePaths = [...new Set(templates.map(config => `${get(config, 'source', 'src')}/**`))]
 
-            const watchPaths = [
-              'src/**/*.*',
+            const otherPaths = [
+              `src/!(${templatePaths.join('|').replace(/src\//g, '').replace(/\/\*/g, '')})/**`,
               get(config, 'build.tailwind.config', 'tailwind.config.js'),
-              ...new Set(templates.map(config => `${get(config, 'source', 'src')}/**/*.*`)),
-              ...new Set(get(config, 'build.browsersync.watch', []))
+              [...new Set(get(config, 'build.browsersync.watch', []))]
             ]
+
+            // Browsersync
+            const baseDir = templates.map(t => t.destination.path)
 
             const bsOptions = {
               notify: false,
@@ -58,44 +62,45 @@ const self = module.exports = { // eslint-disable-line
               ...get(config, 'build.browsersync', {})
             }
 
-            bs.create()
+            bs.init(bsOptions, () => {})
 
-            bs.init(bsOptions)
+            const templatesWatcher = chokidar.watch(templatePaths)
+            const globalWatcher = chokidar.watch(otherPaths)
 
-            bs.watch(watchPaths)
+            templatesWatcher
               .on('change', async file => {
                 file = file.replace(/\\/g, '/')
                 const fileSource = get(templates.filter(v => path.dirname(file).replace(/\\/g, '/').includes(v.source))[0], 'source')
 
-                // Only if this file is in one of the build.templates.source paths
-                if (templates.map(o => o.source).includes(fileSource)) {
-                  const start = new Date()
-                  const spinner = ora('Building email...').start()
+                const start = new Date()
+                const spinner = ora('Building email...').start()
 
-                  const destination = get(templates.filter(v => path.dirname(file).replace(/\\/g, '/').includes(v.source))[0], 'destination.path')
-                  const cssString = fs.existsSync(get(config, 'build.tailwind.css')) ? fs.readFileSync(get(config, 'build.tailwind.css')) : '@tailwind components; @tailwind utilities;'
+                const destination = get(templates.filter(t => path.dirname(file).replace(/\\/g, '/').includes(t.source))[0], 'destination.path')
+                const cssString = fs.existsSync(get(config, 'build.tailwind.css')) ? fs.readFileSync(get(config, 'build.tailwind.css'), 'utf8') : '@tailwind components; @tailwind utilities;'
 
-                  if (config.events && typeof config.events.beforeCreate === 'function') {
-                    await config.events.beforeCreate(config)
-                  }
-
-                  await self.render(await fs.readFile(file, 'utf8'), {
-                    maizzle: config,
-                    tailwind: {
-                      css: cssString
-                    },
-                    ...config.events
-                  })
-                    .then(async ({html}) => {
-                      await fs.outputFile(path.join(destination, file.replace(fileSource, '')), html)
-                        .then(() => {
-                          bs.reload()
-                          spinner.succeed(`Done in ${Date.now() - start} ms.`)
-                        })
-                    })
-                } else {
-                  await self.build('local', config).then(() => bs.reload())
+                if (config.events && typeof config.events.beforeCreate === 'function') {
+                  await config.events.beforeCreate(config)
                 }
+
+                await self.render(await fs.readFile(file, 'utf8'), {
+                  maizzle: config,
+                  tailwind: {
+                    css: cssString
+                  },
+                  ...config.events
+                })
+                  .then(async ({html}) => {
+                    await fs.outputFile(path.join(destination, file.replace(fileSource, '')), html)
+                      .then(() => {
+                        bs.reload()
+                        spinner.succeed(`Done in ${Date.now() - start} ms.`)
+                      })
+                  })
+              })
+
+            globalWatcher
+              .on('change', async () => {
+                await self.build('local', config).then(() => bs.reload())
               })
           })
       })

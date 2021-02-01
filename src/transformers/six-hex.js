a123bad2d4649e93d41ba8ce8aab1e7b82005832
Cosmin Popovici <hellocosmin@gmail.com>
2021-02-01 16:54:53 +0200

    fix(transformers): sixHex import

--- src/transformers/six-hex.js
+++ src/transformers/six-hex.js
@@ -1,10 +1,10 @@
 const {get} = require('lodash')
-const sixHex = require('color-shorthand-hex-to-six-digit')
+const {conv} = require('color-shorthand-hex-to-six-digit')
 
 module.exports = async (html, config = {}) => {
   if (get(config, 'sixHex') === false) {
     return html
   }
 
-  return sixHex(html)
+  return conv(html)
 }

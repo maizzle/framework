7b7bca6577937bf50e5764e3c8945ed2f8fd6c95
Cosmin Popovici <hellocosmin@gmail.com>
2021-06-28 22:06:58 +0300

    fix: compiling css in template with postcss
    make <style postcss> work again

--- src/transformers/transform.js
+++ src/transformers/transform.js
@@ -1,13 +1,29 @@
-const {get} = require('lodash')
+const path = require('path')
 const posthtml = require('posthtml')
 const posthtmlContent = require('posthtml-content')
 const Tailwind = require('../generators/tailwindcss')
+const {requireUncached} = require('../utils/helpers')
+const {get, isObject, isEmpty, merge, omit} = require('lodash')
 
 module.exports = async (html, config = {}, direct = false) => {
   const replacements = direct ? config : get(config, 'transform', {})
   const posthtmlOptions = get(config, 'build.posthtml.options', {})
 
-  replacements.postcss = css => Tailwind.compile(css, html, {}, config)
+  /**
+   * Compile CSS in <style postcss> tags with PostCSS and Tailwind CSS
+   */
+  const maizzleConfig = omit(config, ['build.tailwind.css', 'css'])
+  const tailwindConfig = get(config, 'build.tailwind.config', 'tailwind.config.js')
+  let tailwindObject = (isObject(tailwindConfig) && !isEmpty(tailwindConfig)) ? tailwindConfig : requireUncached(path.resolve(process.cwd(), tailwindConfig))
+  // Use JIT by default, for faster compilation
+  tailwindObject = merge(tailwindObject, {mode: 'jit'})
+
+  replacements.postcss = css => Tailwind.compile(
+    `@tailwind components; @tailwind utilities; ${css}`,
+    html,
+    tailwindObject,
+    maizzleConfig
+  )
 
   return posthtml([posthtmlContent(replacements)]).process(html, posthtmlOptions).then(result => result.html)
 }

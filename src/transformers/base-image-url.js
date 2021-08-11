642f14bdada3427aabcdc9077851523d3394d15e
Cosmin Popovici <hellocosmin@gmail.com>
2021-08-11 12:06:48 +0300

    fix: baseImageURL regex

--- src/transformers/base-image-url.js
+++ src/transformers/base-image-url.js
@@ -2,13 +2,7 @@ module.exports = async (html, config = {}, direct = false) => {
   const url = direct ? config : config.baseImageURL
 
   if (url) {
-    // HTML Attributes
-    html = html.replace(/([^"'=\s]+[^url(]\.(?:jpe?g|a?png|avif|gif|svg|webp|jfif|pjpeg|pjp))/gi, url + '$1')
-
-    // Inline CSS
-    html = html.replace(/(background(-image)?:\s?url\('?)(?![')]|https?:\/\/)\/?/gi, '$1' + url)
-
-    return html
+    return html.replace(/([^"'=\s(]+\.(?:jpe?g|a?png|avif|gif|svg|webp|jfif|pjpeg|pjp))/gi, url + '$1')
   }
 
   return html

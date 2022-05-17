76b71a65b0b231bebf9f51d658f02d5a7315d9c7
Cosmin Popovici <hellocosmin@gmail.com>
2022-05-17 18:25:38 +0300

    feat: default prettify config

--- src/transformers/prettify.js
+++ src/transformers/prettify.js
@@ -1,15 +1,24 @@
+/* eslint-disable camelcase */
+
 const pretty = require('pretty')
-const {get, isEmpty} = require('lodash')
+const {get, merge, isEmpty} = require('lodash')
 
 module.exports = async (html, config = {}, direct = false) => {
   if (get(config, 'prettify') === false) {
     return html
   }
 
-  config = direct ? config : get(config, 'prettify', {})
+  const defaultConfig = {
+    space_around_combinator: true, // Preserve space around CSS selector combinators
+    newline_between_rules: false, // Remove empty lines between CSS rules
+    indent_inner_html: false, // Helps reduce file size
+    extra_liners: [] // Don't add extra new line before any tag
+  }
+
+  config = direct ? config : merge(defaultConfig, get(config, 'prettify', {}))
 
   if (typeof config === 'boolean' && config) {
-    return pretty(html)
+    return pretty(html, defaultConfig)
   }
 
   if (!isEmpty(config)) {

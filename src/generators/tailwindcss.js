cd9d629febba3a33c00300b48eb69c43dcd87a0e
Cosmin Popovici <hellocosmin@gmail.com>
2022-10-21 13:10:26 +0300

    refactor: postcss from path
    makes @config work in css files

--- src/generators/tailwindcss.js
+++ src/generators/tailwindcss.js
@@ -113,7 +113,7 @@ module.exports = {
     }
 
     return postcss([...toProcess])
-      .process(css, {from: undefined})
+      .process(css, {from: userFileExists ? userFilePath : undefined})
       .then(result => result.css)
       .catch(error => {
         throw new SyntaxError(error)

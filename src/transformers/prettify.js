a1be18b201418970ad41311a22d541cda72968cb
Cosmin Popovici <hellocosmin@gmail.com>
2024-01-26 17:30:08 +0200

    fix: prettify formatting issues
    - default embedded lang formatting
    - rewrite closing slash only for known self closing tags
    - fix style="" attr values broken on multiple lines
    - fix broken <pre\n> closing tags
    - undo ' escaping in attr values
    - fix some attrs and closing > broken on a separate line

--- src/transformers/prettify.js
+++ src/transformers/prettify.js
@@ -7,9 +7,25 @@ module.exports = async (html, config = {}, direct = false) => {
   const defaultConfig = {
     parser: 'html',
     printWidth: 500,
-    embeddedLanguageFormatting: 'off',
     htmlWhitespaceSensitivity: 'ignore',
-    xmlMode: get(config, 'posthtml.options.xmlMode', false)
+    xmlMode: get(config, 'posthtml.options.xmlMode', false),
+    rewriteSelfClosing: true,
+    selfClosingTags: [
+      'area',
+      'base',
+      'br',
+      'col',
+      'embed',
+      'hr',
+      'img',
+      'input',
+      'link',
+      'meta',
+      'param',
+      'source',
+      'track',
+      'wbr'
+    ]
   }
 
   // Don't prettify if not explicitly enabled in config
@@ -27,9 +43,27 @@ module.exports = async (html, config = {}, direct = false) => {
 }
 
 const reFormat = (html, config) => {
-  if (/<!doctype html>/i.test(html) && !config.xmlMode) {
-    html = html.replace(/<(.+?)(\s\/)>/g, '<$1>')
+  if (/<!doctype html>/i.test(html) && !config.xmlMode && config.rewriteSelfClosing) {
+    html = html.replace(new RegExp(`<(${config.selfClosingTags.join('|')})\s?([^>]*?)\s?\/>`, 'g'), (match, p1, p2) => {
+      return `<${p1}${p2.trimEnd()}>`
+    })
   }
 
   return html
+    // Fix style="" attributes broken down on multiple lines
+    .replace(/(\s+style="\s+)([\s\S]*?)(\s+")/g, (match, p1, p2, p3) => {
+      return p1.replace(/\n\s+?(style)/g, ' $1').trimEnd()
+        + p2.replace(/\s+/g, ' ').trim()
+        + p3.trim()
+    })
+    // Fix closing </pre> tags broken down on multiple lines (</pre>\n\s+>)
+    .replace(/(<\/pre)\s+>/g, '$1>')
+    // Undo escaping of quotes in attribute values
+    .replace(/="(.*?)"/g, (match, p1) => {
+      return `="${p1.replace(/&quot;/g, '\'')}"`
+    })
+    // Fix <tag \n\s+{attrs}\n\s+> => <tag {attrs}>
+    .replace(/<([^>]+)\n\s*([^>]+)\n\s*>/g, '<$1 $2>')
+    // Fix <tag {attrs}\n[\s\t]*> => <tag {attrs}>
+    .replace(/<([^>]+)\n\s*>/g, '<$1>')
 }

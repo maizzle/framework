5f15dcb808804167f2b23bdceeada26a84c6f0c5
Cosmin Popovici <hellocosmin@gmail.com>
2024-11-04 14:54:58 +0200

    feat: render php tags as-is

--- src/posthtml/defaultComponentsConfig.js
+++ src/posthtml/defaultComponentsConfig.js
@@ -1,11 +1,16 @@
 export default {
   root: './',
-  folders: ['src/components', 'src/layouts', 'src/templates'],
-  fileExtension: 'html',
   tag: 'component',
+  fileExtension: 'html',
+  folders: ['src/components', 'src/layouts', 'src/templates'],
   expressions: {
     loopTags: ['each', 'for'],
     missingLocal: '{local}',
     strictMode: false,
+  },
+  parserOptions: {
+    directives: [
+      { name: '?php', start: '<', end: '>' },
+    ]
   },
 }

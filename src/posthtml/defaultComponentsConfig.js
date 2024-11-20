38e0ed474d73c88d5c458806b32f373819043d42
Cosmin Popovici <hellocosmin@gmail.com>
2024-11-20 18:22:25 +0200

    feat: scan additional components folders
    support new project starter structure, with backwards compatibility

--- src/posthtml/defaultComponentsConfig.js
+++ src/posthtml/defaultComponentsConfig.js
@@ -2,7 +2,15 @@ export default {
   root: './',
   tag: 'component',
   fileExtension: 'html',
-  folders: ['src/components', 'src/layouts', 'src/templates'],
+  folders: [
+    'layouts',
+    'emails',
+    'templates',
+    'components',
+    'src/layouts',
+    'src/templates',
+    'src/components'
+  ],
   expressions: {
     loopTags: ['each', 'for'],
     missingLocal: '{local}',

5c1ed4a88a9194ef6c6a50acdc48ed78749d36c5
Cosmin Popovici <hellocosmin@gmail.com>
2026-04-08 19:15:29 +0300

    feat: preview text component
    or as email people call it, preheader

---
+++ src/components/Preview.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  /** Number of `&#8199;&#847;` filler pairs to render. */
+  fillerCount: {
+    type: Number,
+    default: 150
+  },
+  /** Number of `&shy;` entities to render. */
+  shyCount: {
+    type: Number,
+    default: 150
+  }
+})
+</script>
+
+<template>
+  <Teleport to="body:start">
+    <div style="display:none"><slot /><template v-for="i in fillerCount" :key="'f'+i">&#8199;&#847; </template><template v-for="i in shyCount" :key="'s'+i">&shy; </template>&nbsp;</div>
+  </Teleport>
+</template>

7fcb507c81dba76d74063fad35808868e8c5ec27
Cosmin Popovici <hellocosmin@gmail.com>
2026-04-14 20:11:47 +0300

    feat: text component
    renders a <p> tag with default spacing and typography

---
+++ src/components/Text.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed, useAttrs } from 'vue'
+import { twMerge } from 'tailwind-merge'
+
+defineOptions({ inheritAttrs: false })
+
+const attrs = useAttrs()
+
+const mergedClass = computed(() => twMerge('m-0 my-4 text-base', attrs.class as string))
+</script>
+
+<template>
+  <p v-bind="$attrs" :class="mergedClass">
+    <slot />
+  </p>
+</template>

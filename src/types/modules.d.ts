b9a045c6ab2a28f9acee107aecea26e61c515d99
Cosmin Popovici <hellocosmin@gmail.com>
2026-04-30 13:25:13 +0300

    chore: declare *.vue module shim for tsc

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -3,3 +3,9 @@ declare module 'postcss-sort-media-queries' {
   function sortMediaQueries(options?: { sort?: 'mobile-first' | 'desktop-first' | ((a: string, b: string) => number) }): postcss.Plugin
   export = sortMediaQueries
 }
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}

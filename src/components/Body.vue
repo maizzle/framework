1f89604d26f424397ae7f522ac6c3b748a294a25
Cosmin Popovici <hellocosmin@gmail.com>
2026-05-14 12:24:00 +0300

    chore: add height css on body component

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -94,7 +94,7 @@ const render = () => {
 
   const parts = [
     `dir="${props.dir}"`,
-    'style="margin: 0; padding: 0; width: 100%; word-break: break-word;"',
+    'style="margin: 0; padding: 0; width: 100%; height: 100%; word-break: break-word;"',
   ]
   if (outlookFallback) {
     parts.unshift(`xml:lang="${lang}"`)
